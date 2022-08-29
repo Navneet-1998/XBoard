6401572ffb2eb0cabc5e836d1537a1c5ad33e619
CrioUser <crio-user@criodo.com>
2022-08-29 07:45:16 +0530

    ME_BUILDOUT_XBOARD_MODULE_XBOARD

--- resources/data/magazines.js
+++ resources/data/magazines.js
@@ -1,4 +1,4 @@
-magazines = [
+let magazines = [
   "https://flipboard.com/@thenewsdesk/the-latest-on-coronavirus-covid-19-t82no8kmz.rss",
   "https://flipboard.com/@dfletcher/india-tech-b2meqpd6z.rss",
   "https://flipboard.com/@thehindu/sportstarlive-rj3ttinvz.rss"
@@ -14,3 +14,4 @@ magazines = [
 //   "https://assessment-rss.s3.ap-south-1.amazonaws.com/india-tech.rss",
 //   "https://assessment-rss.s3.ap-south-1.amazonaws.com/sports-star.rss",
 // ];
+
